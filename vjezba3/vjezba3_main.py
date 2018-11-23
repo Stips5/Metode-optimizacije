e132e73254ce7c95a5349439cc9777148f868028
Stips5 <Stips5@users.noreply.github.com>
2018-11-23 19:20:59 +0100

    Add files via upload

---
+++ vjezba3/vjezba3_main.py
@@ -0,0 +1,11 @@
+""" created by stips on 23.11.18. using PyCharm , python_version = 3.5 """
+
+from vjezba3.medak_stipan_zadatak1 import *
+from vjezba3.medak_stipan_zadatak2 import *
+from vjezba3.medak_stipan_zadatak3 import *
+
+if __name__ == '__main__':
+    # zadatak1()
+    zadatak2(2006)
+    # zadatak3()
+    # pass
