4e75d72abe729d791d66355df60f2caee0472021
Stips5 <Stips5@users.noreply.github.com>
2018-11-24 11:34:15 +0100

    Add files via upload
    done

--- vjezba3/medak_stipan_zadatak2.py
+++ vjezba3/medak_stipan_zadatak2.py
@@ -2,6 +2,7 @@
 
 import pandas as panda
 
+
 def zadatak2(godina):
     '''
         2. Napisati funkciju koja će za odredenu godinu (godina je parametar funkcije) sortirati
@@ -11,29 +12,12 @@ def zadatak2(godina):
 
     df = panda.read_csv("svaki_10ti_bez_Nan.csv")
 
-    rowNum = df.shape[0]
-    lastYear = int(df.columns[-1])
-    firstYear = int(df.columns[1])
-
-    print("lastYear", lastYear, " ", "firstYear", firstYear)
-
-    # for county in range(rowNum):
-    #     print(df["Life expectancy"][county])
-
     try:
-
-    df.sort_values(str(godina), ascending=False, inplace=True)
-    print(df[str(godina)])
-
-    '''uradit jos da ostavi samo drzave i OŽV po kojima je sortiran
-    sad sortira dobro al ostavi sve ostale tj cili DataFrame
-    i provjeri jel godina koja se unese u DataFrameu'''
-
-    # print(df)
-    # print(df[str(godina)])
-    #
-    # if godina >= 1800 and godina <= 2016:
-    #     return
-    #
-    # for year in range(1800, 2016):
-    #     print(df[str(year)])
+        '''
+            sortiranje po godini
+            ako godina koja je unesena ne postoji ide na except
+        '''
+        df.sort_values(str(godina), ascending=False, inplace=True)
+        df.to_csv("sortiranoPoGodini.csv")
+    except:
+        print("Godina ne postoji u fileu")
