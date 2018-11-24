1bfc5a1e921e6f6822b0988def89f50cb2eefba7
Stips5 <Stips5@users.noreply.github.com>
2018-11-24 21:16:21 +0100

    Add files via upload
    gotovo

--- vjezba3/vjezba3_main.py
+++ vjezba3/vjezba3_main.py
@@ -1,11 +1,30 @@
 """ created by stips on 23.11.18. using PyCharm , python_version = 3.5 """
 
+import time
+
 from vjezba3.medak_stipan_zadatak1 import *
 from vjezba3.medak_stipan_zadatak2 import *
 from vjezba3.medak_stipan_zadatak3 import *
 
+
 if __name__ == '__main__':
     # zadatak1()
-    zadatak2(2006)
+    # zadatak2(2006)
     # zadatak3()
-    # pass
+
+
+    start = time.time()
+    zadatak1()
+    end = time.time()
+
+    print(end-start)
+
+    start = time.time()
+    zadatak2(2006)
+    end = time.time()
+    print(end - start)
+
+    start = time.time()
+    zadatak3()
+    end = time.time()
+    print(end-start)
