5d267933d205a911cc61592c0e68aef2e5f43a55
Stips5 <smedak95@gmail.com>
2018-12-08 22:57:33 +0100

    read done

--- vjezba4/zadatak4_main.py
+++ vjezba4/zadatak4_main.py
@@ -10,25 +10,19 @@ class MatricaSusjedstva:
         self.matrica = None
         self.fileName = fileName
         self.keywords = ("*Vertices", "*Arcs", "*Edges")
+        self.vrhovi = dict()
+        self.edges = list()
+        self.arcovi = list()
 
-        data = self.read(self.fileName)
+        self.read(fileName)
 
     def read(self, fileName):
-        vrhovi = dict()
-        edges = dict()
-        edgevi = list()
-        arcovi = list()
 
         file = open(fileName, "r")
-        '''cita prvi red i gleda koliko ima tocaka
-        pa svaku tocku ucita'''
 
-    #arcs usmjereni , edge veza
+        '''cita prvi red i gleda koliko ima tocaka pa svaku tocku ucita'''
 
-        file1 = open(fileName, "r")
-
-        for i, line in enumerate(file1):
-            print(i, line)
+        #arcs usmjereni , edge veza
 
         for row in file:
             '''
@@ -42,7 +36,7 @@ class MatricaSusjedstva:
                 row = next(file)
                 for i in range(nRows):
                     splited = row.replace("\n", "").replace(" ", "").split('"')
-                    vrhovi[splited[1]] = splited[0]
+                    self.vrhovi[splited[1]] = int(splited[0])
                     row = next(file)
 
             elif row.__contains__(self.keywords[1]):   #lukovi
@@ -51,31 +45,94 @@ class MatricaSusjedstva:
                     if row.__contains__(self.keywords[2]):
                         break
                     else:
-                        arcovi.append(row.replace("\n", ""))
+                        self.arcovi.append(row.replace("\n", ""))
             else:
                 while True:
                     row = next(file, 0)
                     if row == 0:
                         break
                     else:
-                        edgevi.append(row.replace("\n", ""))
+                        # edgevi.append(row.replace("\n", ""))
+                        kae = [int(s) for s in row.split() if s.isdigit()]
+                        self.edges.append(kae[:2])
 
-        print("spremljeno")
+        self.matrica = [[0 for x in range(len(self.vrhovi))] for y in range(len(self.vrhovi))]
 
-        print("Vrhovi")
-        for k, v in vrhovi.items():
+        for e in self.edges:
+            i1, i2 = e[0], e[1]
+            self.matrica[i1-1][i2-1] += 1
+            self.matrica[i2-1][i1-1] += 1
+
+    def print(self):
+        print("Matrica susjedstva")
+        for r in range(5):
+            for c in range(5):
+                print(self.matrica[r][c], end=" ")
+            print()
+
+    def convertToListaSusjedstva(self):
+        ls = ListaSusjedstva()
+
+        for k,v in self.vrhovi.items():
+            ls.lista[v] = None
+
+        for c in range(len(self.matrica[0])):
+            lista = list()
+            for r in range(len(self.matrica[0])):
+                if self.matrica[r][c] == 1:
+                    lista.append(r+1)
+            ls.lista[c+1] = lista
+
+        return ls
+
+    def convertToMatricaIncidencije(self):
+        mi = MatricaIncidencije(self.vrhovi, self.edges)
+
+        for e in range(len(self.edges)):
+            e1, e2 = self.edges[e][0], self.edges[e][1]
+            mi.matrica[e1-1][e] += 1
+            mi.matrica[e2-1][e] += 1
+
+        return mi
+
+class MatricaIncidencije:
+    def __init__(self, vrhovi, edgevi):
+        self.vrh = vrhovi
+        self.edg = edgevi
+        self.matrica = [[0 for x in range(len(edgevi))] for y in range(len(vrhovi))]
+
+    def print(self):
+        print("Matrica incidencije")
+        for r in range(len(self.vrh)):
+            for c in range(len(self.edg)):
+                print(self.matrica[r][c], end=" ")
+            print()
+
+class ListaSusjedstva:
+    def __init__(self):
+        self.lista = dict()
+
+    def print(self):
+        print("Lista susjedstva")
+        for k, v in self.lista.items():
             print(k, v)
 
-        print("Arcovi")
-        for i in arcovi:
-            print(i)
-
-        print("Edgevi")
-        for i in edgevi:
-            print(i)
 
 if __name__ == '__main__':
         # fileName = "euler.net.txt"
         fileName = "football.net.txt"
 
-        MatricaSusjedstva(fileName)
+        ms = MatricaSusjedstva(fileName)
+
+        # print(len(ms.matrica[0]))
+
+        ms.print()
+
+
+        ls = ms.convertToListaSusjedstva()
+
+        ls.print()
+
+
+        mi = ms.convertToMatricaIncidencije()
+        mi.print()
